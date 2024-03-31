40ec2f28d25da8e439fba560bc040c64bc54e4ad
z9877969 <z9877969@ukr.net>
2024-04-01 02:33:31 +0300

    unpack backend files

--- src/frontend/components/Layout/Layout.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const LayoutWrapper = styled.div`
-  width: 1440px;
-  display: grid;
-  padding: 16px;
-  margin: 0 auto;
-`;
--- src/redux/download/selectors.js
+++
@@ -1,1 +0,0 @@
-export const selectIsDownloading = state => state.download.isDownloading;
